ce54e70f2c27f37b441f592476bf613e6d656507
soburi59 <78711203+soburi59@users.noreply.github.com>
2022-08-28 22:22:37 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/library.png') {
+      myImage.setAttribute('src','images/library2.png');
+    } else {
+      myImage.setAttribute('src','images/library.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName(){
+    let myName=prompt('あなたの名前は？');
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem('name',myName);
+        myHeading.textContent = '来てくれてありがとう'+myName;
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStrage.getItem('name');
+    myHeading.innerHTML = '来てくれてありがとう'+storedName;
+}
+
+myButton.onclick=function(){
+    setUserName();
+}

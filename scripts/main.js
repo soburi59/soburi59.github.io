ca0b2fda665a096f33aa2af2d41af1e3f2320a2f
soburi59 <78711203+soburi59@users.noreply.github.com>
2022-08-30 03:03:10 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
-let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+let myImage = document.querySelector('.logo');
+
+myImage.addEventListener('mouseover',function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/library.png') {
-      myImage.setAttribute('src','images/library2.png');
+    if(mySrc === 'images/logo.png') {
+      myImage.setAttribute('src','images/logo-hover.png');
     } else {
-      myImage.setAttribute('src','images/library.png');
+      myImage.setAttribute('src','images/logo.png');
     }
-}
+});
+myImage.addEventListener('mouseleave',function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/logo.png') {
+      myImage.setAttribute('src','images/logo-hover.png');
+    } else {
+      myImage.setAttribute('src','images/logo.png');
+    }
+});
+
 /*
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
